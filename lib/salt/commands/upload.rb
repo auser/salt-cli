bd9ac4bd38bed2958e70dc0eb4c1730874267877
Ari Lerner <arilerner@mac.com>
2013-05-23 00:40:05 -0700

    Separated sync from upload -s to sync and upload

--- lib/salt/commands/upload.rb
+++ lib/salt/commands/upload.rb
@@ -8,12 +8,13 @@ module Salt
         remotepath  = remote || "/srv/salt"
         dsystem sudo_cmd(vm, "sudo mkdir -p #{remotepath} && sudo chown #{vm.user} #{remotepath}")
         dsystem rsync_cmd(vm, localpath, remotepath)
-        salt_cmd vm, 'saltutil.sync_all'
+        salt_cmd vm, 'saltutil.sync_all' if sync
       end
       
       def self.additional_options(x)
         x.on("-l", "--local <directory>", "Local directory") {|n| config[:local] = n}
         x.on("-r", "--remote <directory>", "Remote directory") {|n| config[:remote] = n}
+        x.on('-s', '--sync', "Sync after upload") { config[:sync] = true }
       end
       
     end
