0c1bb5681b8a0e9cad213af01adc640c122b1499
Ari Lerner <arilerner@mac.com>
2013-06-07 09:11:03 -0700

    Fixed the AWS key

--- lib/salt/providers/aws_provider.rb
+++ lib/salt/providers/aws_provider.rb
@@ -205,7 +205,7 @@ module Salt
       end
       
       def aws_secret_access_key
-        ENV["AWS_ACCESS_KEY"] || aws[:secret_key]
+        ENV["AWS_SECRET_ACCESS_KEY"] || aws[:secret_key]
       end
       
     end
